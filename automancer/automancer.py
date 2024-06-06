d164105a27ce34ac4a5cacff1e2be98383a6617f
pretentiousUsername <mitchell.ian.2001@gmail.com>
2024-06-05 23:48:38 -0400

    update code formatting

--- automancer/automancer.py
+++ automancer/automancer.py
@@ -1,24 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import os
+
 
 # I'll be assuming that all things in a cellular automata simulation are just
 # numbers.
 class CellularAutomata:
-    def __init__(self, grid, baseline = 0):
+    def __init__(self, grid, baseline=0):
         if len(grid) != 2:
             raise ValueError("Grid must be iterable with length 2.")
         else:
-            self.grid = grid # TODO: get error handling for grids with a size greater than two
+            self.grid = grid  # TODO: get error handling for grids with a size greater than two
         self.state = np.zeros(grid)
-        self.rules = [] # initialize a set of rules
+        self.rules = []  # initialize a set of rules
         self.neighborhoods = []
         self.baseline = baseline
 
     def __str__(self):
         return f"CellularAutomata ({self.grid[0]}x{self.grid[1]}) \n {self.state}"
+
     def __repr__(self):
         return f'CellularAutomata({self.grid})'
+
     # TODO: change this to get rid of redundant updates (e.g. an updating the same
     # cell twice), rather than just looking at the state of the grid, which should
     # be handled when defining a rule
@@ -29,17 +31,18 @@ class CellularAutomata:
             return True
         else:
             return False
+
     # from https://pymorton.wordpress.com/2015/02/16/wrap-integer-values-to-fixed-range/
     def __keep_periodic(self, number, maximum):
         return (number) % (maximum)
-    
+
     def set_state(self, state):
         self.state = np.flip(state)
 
     # Is this really the best way to update the grid?
     # Anyways, this method is **fundamental**---don't get it spaghettified.
     # new_values is a list of tuples (e.g. `[((0, 0), 1)]`)
-    def update(self, new_values): 
+    def update(self, new_values):
         for item in new_values:
             i, j = item[0]
             value = item[1]
@@ -48,9 +51,9 @@ class CellularAutomata:
     # This method needs some serious refinement
     def build_neighborhood(self, size, shape = 'v'):
         size += 1
-        area = range(-size, size + 1) #range(-size, size + 1)
+        area = range(-size, size + 1)
         neighborhood = []
-        if shape == 'v' or shape == 'Von Neumann': # Von Neumann neighborhood
+        if shape == 'v' or shape == 'Von Neumann':  # Von Neumann neighborhood
             for i in area:
                 for j in area:
                     if np.abs(i) + np.abs(j) <= size:
@@ -61,23 +64,23 @@ class CellularAutomata:
         elif shape == 'm' or shape == 'Moore':
             for i in area:
                 for j in area:
-                   if (np.abs(i) <= size) and (np.abs(j) <= size):
-                       k = (i, j)
-                       neighborhood.append(k)
-                   else:
-                       pass
+                    if (np.abs(i) <= size) and (np.abs(j) <= size):
+                        k = (i, j)
+                        neighborhood.append(k)
+                    else:
+                        pass
         else:
             raise ValueError("Neighborhood must be either Moore of Von Neumann")
         return neighborhood
 
     def custom_neighborhood(self, neighborhood):
-        neighborhoods.append(neighborhood)
+        self.neighborhoods.append(neighborhood)
 
     def add_neighborhood(self, external = None, **kwargs):
         custom = kwargs.pop('custom' or 'c', False)
         shape  = kwargs.pop('shape' or 's', 'v')
-        size   = kwargs.pop('size'  or 'a', 1)
-        if custom == True and external != None:
+        size   = kwargs.pop('size' or 'a', 1)
+        if custom is True and external is not None:
             self.neighborhoods.append(external)
         else:
             a = self.build_neighborhood(size - 1, shape)
@@ -90,22 +93,22 @@ class CellularAutomata:
     def generate_picture(self, **kwargs):
         file = kwargs.pop('file', None)
         fig, ax = plt.subplots()
-        
+
         # this feels bad to write :|
-        x = np.arange(0, self.grid[0]) # REALLY mull over using `range` vs.
-        y = np.arange(0, self.grid[1]) # `np.arange`
+        x = np.arange(0, self.grid[0])  # REALLY mull over using `range` vs.
+        y = np.arange(0, self.grid[1])  # `np.arange`
         ax.xaxis.set_major_locator(MaxNLocator(integer=True))
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
         z = self.state
         ax.pcolormesh(x, y, z)
-        
-        if file != None:
+
+        if file is not None:
             plt.savefig(file)
         else:
             plt.show()
 
     def add_rule(self, rule):
-        if type(rule) == str:
+        if type(rule) is str:
             raise TypeError("Input rule must be a function.")
         elif hasattr(rule, '__iter__'):
             self.rules.append(r if callable(r) else None for r in rule)
@@ -149,8 +152,8 @@ class CellularAutomata:
         for t in time:
             self.evaluate_rules()
             evolution.append(self.state)
-        
-        if file == None:
+
+        if file is None:
             return evolution
         else:
             np.save(file, evolution)
@@ -158,20 +161,21 @@ class CellularAutomata:
     def generate_picture(self, **kwargs):
         file = kwargs.pop('file', None)
         fig, ax = plt.subplots()
-        
+
         # this feels bad to write :|
-        x = np.arange(0, self.grid[0]) # REALLY mull over using `range` vs.
-        y = np.arange(0, self.grid[1]) # `np.arange`
+        x = np.arange(0, self.grid[0])  # REALLY mull over using `range` vs.
+        y = np.arange(0, self.grid[1])  # `np.arange`
         z = self.state
         ax.pcolormesh(x, y, z)
-        
+
         if file != None:
             plt.savefig(file)
         else:
             plt.show()
-    
+
     def energy(self):
         return np.sum(self.state)
+
 
 def move_point(state, point, amount):
     temp_state = np.zeros(np.shape(state))
@@ -186,6 +190,7 @@ def move_point(state, point, amount):
             update.append([tuple(p), 1])
         return update
 
+
 def count_neighbors(state, neighborhood, point):
     neighborhood = [n for n in neighborhood if n != (0, 0)]
     neighbors = 0
@@ -193,7 +198,8 @@ def count_neighbors(state, neighborhood, point):
         temp_state = np.roll(state, shift = n, axis = (0, 1))
         neighbors += temp_state[point]
     return neighbors
-    
+
+
 # rules must have the state, neighborhood, and point evaluated in that order.
 # rule must output a list of the form [[(i, j), value]]
 def game_of_life(state, neighborhood, point):
