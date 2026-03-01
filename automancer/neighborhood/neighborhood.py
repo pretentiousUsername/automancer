1492d2aa780f39ea714379f858872ee8ec41c231
Ian Mitchell <mitchell.ian.2001@gmail.com>
2026-03-01 14:57:53 -0500

    get some work done

--- automancer/neighborhood/neighborhood.py
+++ automancer/neighborhood/neighborhood.py
@@ -1,7 +1,29 @@
+import numpy as np
+
+def Help():
+    ...
+
 class Neighborhood():
-    def __init__(self, size):
-        self.size = self.__check_size(size)
+    def __init__(self):
         self.points = []
+
+    def automata_points(self):
+        ...
+
+
+class DefinedNeighborhood(Neighborhood):
+    def __init__(self):
+        super().__init__()
+
+    def __in_area(self):
+        ...
+
+class DefinedNeighborhood2d(Neighborhood):
+    def __init__(self, size):
+        super().__init__()
+        self.size = self.__check_size(size)
+        self.area = np.arange(-self.size, self.size + 1)
+        self.points = self.generate_points()
 
     def __check_size(self, size):
         if size >= 1:
@@ -12,47 +34,39 @@ class Neighborhood():
     def build_neighborhood(self):
         ...
 
-    def points(self):
-        ...
-
-        size += 1
-        area = np.arange(-size, size + 1)
-        neighborhood = []
-        if shape == 'v' or shape == 'Von Neumann':  # Von Neumann neighborhood
-            for i in area:
-                for j in area:
-                    if np.abs(i) + np.abs(j) <= size:
-                        k = (i, j)
-                        neighborhood.append(k)
-                    else:
-                        pass
-        elif shape == 'm' or shape == 'Moore':
-            for i in area:
-                for j in area:
-                    if (np.abs(i) <= size) and (np.abs(j) <= size):
-                        k = (i, j)
-                        neighborhood.append(k)
-                    else:
-                        pass
-        else:
-            raise ValueError("Neighborhood must be either Moore of Von Neumann")
-        return neighborhood
+    def generate_points(self):
+        points = []
+        for i in self.area:
+            for j in self.area:
+                points.append((i, j))
+        return np.array(points)
 
 
-class MooreNeighborhood(Neighborhood):
+class MooreNeighborhood(DefinedNeighborhood2d):
     def __init__(self):
         super().__init__(self)
-        self.area = np.arange(-size, size + 1)
+
+    def __in_area(self, i, j):
+        return (np.abs(i) <= self.size) and (np.abs(j) <= self.size)
+
+    def build_neighborhood(self):
+        for i in self.area:
+            for j in self.area:
+                if (np.abs(i) <= size) and (np.abs(j) <= size):
+                    k = (i, j)
+                    self.points.append(k)
 
 
-class VonNeumannNeighborhood(Neighborhood):
+class VonNeumannNeighborhood(DefinedNeighborhood2d):
     def __init__(self):
         super().__init__(self)
-        self.area = np.arange(-size, size + 1)
+
+    def __in_area(self, point):
+        return np.abs(point[0]) + np.abs(point[1]) <= self.size
 
     def build_neighborhood(self):
         for i in self.area:
             for j in self.area:
                 if np.abs(i) + np.abs(j) <= size:
                     k = (i, j)
-                    neighborhood.append(k)
+                    self.points.append(k)
